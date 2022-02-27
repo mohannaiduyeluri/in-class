1696ea8e833461d64e043db4c2d51ebb5d7eff1d
mohannaiduyeluri <mohansandeep.yeluri@gmail.com>
2022-02-27 17:15:15 -0500

    Added changes for c;ient using vue js jitejs.dev

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue';
+    
+
+</script>
+
+<template>
+    <Nav />
+    <h1 class="title">
+        Praying Ukraine - Праздники в Украине
+    </h1>
+    <Messages />
+    
+</template>
+
+<style>
+
+</style>
